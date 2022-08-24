5355d6fab58e5700cdab6428336a9c17536179ee
Nur Kholid <cocokcocok33@gmail.com>
2022-08-24 14:49:22 +0700

    test

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,9 @@
+import sveltePreprocess from "svelte-preprocess";
 import adapter from "@sveltejs/adapter-netlify";
-
 export default {
   // Consult https://github.com/sveltejs/svelte-preprocess
   // for more information about preprocessors
+  preprocess: sveltePreprocess(),
   kit: {
     adapter: adapter(),
   },
